216a01f6d98f7a21e2b183b0038bd3ece66faaa7
Shubham Jain <jshubham94110@gmail.com>
2021-06-26 23:29:19 +0530

    Intial

---
+++ main.js
@@ -0,0 +1,12 @@
+function onScanSuccess(decodedText, decodedResult) {
+    // Handle on success condition with the decoded text or result.
+    console.log(`Scan result: ${decodedText}`, decodedResult);
+}
+
+function onScanError(errorMessage) {
+    // handle on error condition, with error message
+}
+
+var html5QrcodeScanner = new Html5QrcodeScanner(
+    "qr-reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess, onScanError);
